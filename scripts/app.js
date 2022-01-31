8e0aa35478d91f4265e50802e34dc0e5846bf19c
Opilane TTHK <opilane@tthk.ee>
2022-01-31 17:53:58 +0200

    add files

---
+++ scripts/app.js
@@ -0,0 +1,9 @@
+//console.log("hello from script");
+
+const myButton = document.querySelector("button");
+const myImage = document.querySelector("img");
+
+myButton.addEventListener("click",()=> {
+console.log("click");
+myImage.src="images/pic5.jpeg";
+});
