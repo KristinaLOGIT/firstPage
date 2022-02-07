c9d72e446dd8225d5a64fadb1ffb9badfe8b9c2d
Opilane TTHK <opilane@tthk.ee>
2022-02-07 18:06:08 +0200

    add gallery

--- scripts/app.js
+++ scripts/app.js
@@ -2,8 +2,18 @@
 
 const myButton = document.querySelector("button");
 const myImage = document.querySelector("img");
+const images = ["pilt1", "pilt2", "pilt3", "pilt4", "pilt5" ];
 
 myButton.addEventListener("click",()=> {
 console.log("click");
-myImage.src="images/pilt5.jpeg";
+
+//generate a random number from 0 to 4
+
+let randomIndex = Math.floor(Math.random()*images.length);
+console.log(randomIndex);
+
+let randomImagePath = "images/" + images [randomIndex] + ".jpg"
+console.log(randomImagePath);
+
+myImage.src=randomImagePath;
 });
